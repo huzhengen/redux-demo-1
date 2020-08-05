a421d559978cbd2bd9d21970a7f084ffdc691658
hukai <huzhengen@163.com>
2020-08-05 14:51:23 +0800

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+import store from './store'
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = store.getState();
+  }
+
+  render() {
+    return (
+      <div style={{ margin: '10px' }}>
+        <div>
+          <Input placeholder='write someting' style={{ width: '250px', marginRight: '10px' }} />
+          <Button type="primary">增加</Button>
+        </div>
+        <div style={{ margin: '10px', width: '300px' }}>
+          <List
+            bordered
+            dataSource={this.state.list}
+            renderItem={item => (<List.Item>{item}</List.Item>)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+export default TodoList;
