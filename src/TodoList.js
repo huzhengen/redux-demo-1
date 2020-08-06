4b253ceaf7d4ec893c37e7065dbaeec4a5c80c5a
hukai <huzhengen@163.com>
2020-08-06 17:32:12 +0800

    TodoListUI

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css'
-import { Input, Button, List } from 'antd'
 import store from './store'
 import { changeInputAction, addItemAction, deleteItemAction } from './store/actionCreators'
+import TodoListUI from './TodoListUI'
 
 class TodoList extends Component {
   constructor(props) {
@@ -26,32 +25,20 @@ class TodoList extends Component {
   }
 
   deleteItem = index => {
+    console.log(index);
     const action = deleteItemAction(index)
     store.dispatch(action)
   }
 
   render() {
     return (
-      <div style={{ margin: '10px' }}>
-        <div>
-          <Input
-            placeholder={this.state.inputValue}
-            style={{ width: '250px', marginRight: '10px' }}
-            onChange={this.changeInputValue}
-          />
-          <Button
-            type="primary"
-            onClick={this.clickBtn}
-          >增加</Button>
-        </div>
-        <div style={{ margin: '10px', width: '300px' }}>
-          <List
-            bordered
-            dataSource={this.state.list}
-            renderItem={(item, index) => (<List.Item onClick={this.deleteItem.bind(this, index)}>{item}</List.Item>)}
-          />
-        </div>
-      </div>
+      <TodoListUI
+        inputValue={this.state.inputValue}
+        list={this.state.list}
+        changeInputValue={this.changeInputValue}
+        clickBtn={this.clickBtn}
+        deleteItem={this.deleteItem}
+      />
     );
   }
 }
